89da899ce308a3a5aa3f4c4f62f75f293203d3ac
Gkmegh <118533859+Gkmegh@users.noreply.github.com>
2023-07-18 14:44:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function volume_sphere() {
-    //Write your code here
+
+  var radius = document.getElementById('radius').value;
+
+  if (radius === '' || radius <= 0) {
+    alert('Please enter a valid positive radius.');
+    return;
+  }
+  var volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+
+  document.getElementById('volume').value = volume.toFixed(2);
+}
+
+document.getElementById('MyForm').addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevent form submission
   
-} 
+  volume_sphere();
+});
+
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
