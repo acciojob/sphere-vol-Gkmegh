fe90308d5fa0b647e65ea4e50a788a6e969b224c
Gkmegh <118533859+Gkmegh@users.noreply.github.com>
2023-07-18 14:54:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function volume_sphere() {
   }
   var volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
 
-  document.getElementById('volume').value = volume.toFixed(2);
+  document.getElementById('volume').value = volume.toFixed(4);
 }
 
 document.getElementById('MyForm').addEventListener('submit', function(event) {
